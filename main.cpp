a5111f2ca070c7ffa4e93fa7fd48c383f7ff03ea
Jakob Flierl <jakob.flierl@gmail.com>
2026-08-20 13:50:46 +0200

    Bump version to 0.1.29

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ void customMessageHandler(QtMsgType type, const QMessageLogContext &context, con
 #include "mainwin.h"
 #include "g2m.hpp"
 
-#define APP_VERSION QString("0.1.28")
+#define APP_VERSION QString("0.1.29")
 #define APP_NAME QString("gcoder")
 #define APP_NAME_FULL QString("GCoder")
 #define APP_ORGANIZATION QString("gcoder.koppi.github.com")
