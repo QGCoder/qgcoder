98817eba2ce081f00ae77de12812e7fddf28cf7b
Jakob Flierl <jakob.flierl@gmail.com>
2026-09-16 19:07:53 +0200

    Release 0.1.40

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ using namespace Qt::StringLiterals;
 
 namespace {
 
-constexpr auto kAppVersion = "0.1.39";
+constexpr auto kAppVersion = "0.1.40";
 constexpr auto kAppName = "gcoder";
 constexpr auto kAppOrganization = "gcoder.koppi.github.com";
 
